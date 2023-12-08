8adb798b09bf5a25369146d2e4c223281d93cae1
seeun <seeun0426@gmail.com>
2023-12-08 22:38:31 +0900

    feat: 스프링부트 메인클래스 생성 / 2023-12-08 이세은

---
+++ KccBrew/src/main/java/kr/co/kccbrew/KccbrewApplication.java
@@ -0,0 +1,11 @@
+package kr.co.kccbrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KccbrewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KccbrewApplication.class, args);
+    }
+}
