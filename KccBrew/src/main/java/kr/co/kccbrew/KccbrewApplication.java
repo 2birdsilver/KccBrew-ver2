967c1000c5b3bec9de17c7bb1f2ddf998c5aaf55
seeun <seeun0426@gmail.com>
2023-12-13 02:55:52 +0900

    feat: 스프링시큐리티 재설정 / 2023-12-13 이세은

--- KccBrew/src/main/java/kr/co/kccbrew/KccbrewApplication.java
+++ KccBrew/src/main/java/kr/co/kccbrew/KccbrewApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KccbrewApplication {
+public class   KccbrewApplication {
     public static void main(String[] args) {
         SpringApplication.run(KccbrewApplication.class, args);
     }
